17050505e1d559869e7b3df6225dc83b6b0c9976
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-10 22:40:30 +0300

    Added Refs and Forwards Refs

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,43 @@
-import React from "react";
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
+const defaultState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_CART_ITEM"){
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+        items:updatedItems, 
+        totalAmount: updatedTotalAmount
+    };
+  }
+  
+  
+  return defaultState;
+};
+
 const CartProvider = ({ children }) => {
-  const addItemCartHandler = (item) => {};
-  const removeItemHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultState);
+
+  const addItemCartHandler = (item) => {
+    dispatchCartAction({
+      type: "ADD_CART_ITEM",
+      item: item,
+    });
+  };
+  const removeItemHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_CART_ITEM", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemCartHandler,
     removeItem: removeItemHandler,
   };
