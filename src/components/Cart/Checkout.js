e4ce8c520cf68849e7c357e653f90f62fa525036
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-17 00:20:58 +0300

    Added More Data to the Checkout form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,35 @@
-import classes from './Checkout.module.css';
+import classes from "./Checkout.module.css";
 
-const Checkout = ({onCancel}) => {
-
-    const confirmHandler = event => {
-        event.preventDefault();
-    }
-    return <form onSubmit={confirmHandler}>
-        <div className={classes.control}>
-            <label htmlFor='name'>Your Name</label>
-            <input type="text" id="name"></input>
-        </div>
-        <div className={classes.control}>
-            <label htmlFor='street'>Street</label>
-            <input type="text" id="street"></input>
-        </div>
-        <div className={classes.control}>
-            <label htmlFor='postal'>Postal Code</label>
-            <input type="text" id="postal"></input>
-        </div>
-        <div className={classes.control}>
-            <label htmlFor='city'>City</label>
-            <input type="text" id="city"></input>
-        </div>
-        <button type="button" onClick = {onCancel}>Cancel</button>
-        <button>Confirm</button>
-
+const Checkout = ({ onCancel }) => {
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form onSubmit={confirmHandler} className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city"></input>
+      </div>
+      <div className={classes.actions}> 
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
+  );
 };
 
 export default Checkout;
